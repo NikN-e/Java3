820d4d9d6fcf0c1e11373ee48ef4ed3608575c3a
NikN-e <Nikolai1193@gmail.com>
2021-09-14 16:12:19 +0300

    .

--- ArraySwap.java
+++
@@ -1,7 +0,0 @@
-public class ArraySwap {
-    public static void arraySwap(Object[] arr, int index1, int index2){
-        Object tmp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = tmp;
-    }
-}
